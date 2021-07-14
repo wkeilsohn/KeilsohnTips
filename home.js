97012f39ac522c39e3c8a151b40046f2910c4c02
wkeilsohn <kingwilliam42@gmail.com>
2021-07-14 15:20:09 -0400

    Allow users to copy codes

---
+++ home.js
@@ -0,0 +1,16 @@
+function copyCode(wallet_value) {
+  // Define Variabe
+  var copyText = document.getElementById(wallet_value);
+  // Create Area for text to go
+  var input = document.createElement("textArea");
+  // Add variable text to text area
+  input.value = copyText.textContent;
+  document.body.appendChild(input);
+  input.select();
+  input.setSelectionRange(0, 99999); 
+  // Return to user
+  document.execCommand("copy");
+  // Removes the text area and alert the user
+  input.remove();
+  alert("Copied!");
+} 
